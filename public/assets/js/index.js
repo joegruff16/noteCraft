e05b422d8dd801dd1c37edc8ae707264427bae1a
Joseph Ruff <joegruff16@gmail.com>
2024-05-17 15:24:10 -0400

    Updated title in index and note taking HTML files with the correct script file path for the index.js file. Began commenting out my js file to prepare to complete the code to maek the GET functions work

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,18 @@
+// Variables will most likely have to be modified as they aren't storing anything currently
+
+// Figure out where this would go in this js file
+
+// GET /notes should return the notes.html file.
+
+// GET * should return the index.html file.
+
+// The following API routes should be created:
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+
 let noteForm;
 let noteTitle;
 let noteText;
@@ -28,6 +43,7 @@ const hide = (elem) => {
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
 
+// Function currently doesn't work
 const getNotes = () =>
   fetch('/api/notes', {
     method: 'GET',
