e0549beb5b8097c6ed09a62b7b2f371b8f1d26b5
Joseph Ruff <joegruff16@gmail.com>
2024-05-23 22:20:05 -0400

    Moved select all GET as the last route because it was causing issues with the other routes. Created new note variable under POST to save new note and created a unique id for each note

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const fs = require('node:fs');
 const express = require('express');
 const app = express();
 const path = require('path');
+// Create a way to create a unique id for each note in the POST route
+const { v4: uuidv4 } = require('uuid');
 // Create a variable to store our port establishing environment variables
 const PORT = process.env.PORT || 3000
 
 // Variable to store db.json as a file path
-const filePath = 'db.json';
+const filePath = './db/db.json';
 
 // Middleware
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,14 +20,8 @@ app.use(express.json());
 
 app.get('/notes', (req, res) => {
     // GET /notes should return the notes.html file.
+    console.log(`Here is the notes page`)
     res.sendFile(path.join(__dirname, 'public/notes.html'))
-})
-
-app.get("*", (req, res) => {
-    // GET * should return the index.html file.
-    console.log(`Here is the main page that is running`);
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-
 })
 
 app.get('/api/notes', (req, res) => {
@@ -38,20 +34,45 @@ app.get('/api/notes', (req, res) => {
             console.error(err);
             return;
         }
+
         // To take out of a json file is you would need to parse the data that was read out of the json file
         const savedNotes = JSON.parse(data);
         // Return saved notes as JSON
-        console.log(savedNotes);
+
+        // Now it needs to be sent back to the browser with res.json()
+        res.json(savedNotes);
     });
 
-    // Now it needs to be sent back to the browser with res.json()
-    res.json(savedNotes);
+
 })
 
 // POST Route
 app.post('/api/notes', (req, res) => {
     // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
     // What extra steps do I need to take with the GET route above to include into the POST
+    // Step 1 POST/api/notes receives a new note to save on the request body
+    const newNote = req.body;
+
+    // Give each note a unique id
+    newNote.id = uuidv4();
+    console.log(newNote);
+    // Step 2 Add the new note to the db.json file and return the new note to the client
+    // fs.readFileSync
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) throw err
+        console.log(data)
+        data.push(newNote);
+        fs.writeFile(filePath, JSON.stringify(data), (err, data))
+    });
+
+    res.json(newNote);
+
+
+})
+app.get("*", (req, res) => {
+    // GET * should return the index.html file.
+    console.log(`Here is the main page that is running`);
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 
 })
 
